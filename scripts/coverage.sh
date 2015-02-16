8de07e551bf461396a903c6ae3f60e9e1802ebce
Nax (aka Matt) <m.neundorf@live.de>
2015-02-16 09:08:42 +0000

    added codeclimate pack to devDeps, update coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
+BASEDIR=$(dirname $(readlink -f $0))
+
+export NODE_ENV='test'
+export DEBUG='tea-party:tea-party:*'
+export CONFIG="$BASEDIR/../app/config"
+export HELPERS="$BASEDIR/../app/helpers"
+export MODELS="$BASEDIR/../app/models"
+export MODULES="$BASEDIR/../app/helpers/modules"
 
 istanbul cover -x "**/vendor/**" _mocha -- -R spec --recursive ./test
-export CODECLIMATE_REPO_TOKEN=86f5769ffecb56e9cec7a8cc032a5dff38db15ca64e76016
+export CODECLIMATE_REPO_TOKEN=86f5769ffecb56e9cec7a8cc032a5dff38db15ca64e76016fab0efa13e484dbd
 codeclimate < coverage/lcov.info
