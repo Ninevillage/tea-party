d24dd1d5f1070b9ca37dde18e1411214cfbb3f36
Nax (aka Matt) <m.neundorf@live.de>
2015-02-16 08:55:30 +0000

    update codeclimate token

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 istanbul cover -x "**/vendor/**" _mocha -- -R spec --recursive ./test
-export CODECLIMATE_REPO_TOKEN=44e854e9f538b5e8df7ed5e7efc78533999817d9b9a088a7
+export CODECLIMATE_REPO_TOKEN=86f5769ffecb56e9cec7a8cc032a5dff38db15ca64e76016
 codeclimate < coverage/lcov.info
